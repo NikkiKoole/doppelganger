9a6befd2178331d2f7638746fde9c729ba40cd85
Nikki Koole <nikkikoole@gmail.com>
2015-05-19 22:10:28 +0200

    doing some actual rendering in game_update_ande_render

--- game.c
+++ game.c
@@ -1,5 +1,7 @@
 #include <SDL2/SDL.h>
 
 void game_update_and_render(SDL_Renderer *renderer){
-    printf("Running update and renderer.\n");
+    SDL_SetRenderDrawColor( renderer, 0x00, 0xAA, 0xAA, 0xFF );
+    SDL_RenderClear( renderer );
+    SDL_RenderPresent( renderer );
 }
